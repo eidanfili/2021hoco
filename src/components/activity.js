b9a264375df30023852faf84d59d72d5d53cc13a
eidanfili <eidan@ledezma.org>
2021-09-28 23:12:53 -0600

    first commit before heroku

---
+++ src/components/activity.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Line from './line';
+
+const Activity = props => {
+    const {title, time, line} = props;
+    return (
+        <div className="activity">
+            <div className="activity_cont">
+                <div className="activity_title">{title}</div>
+                <div className="activity_time">{time}</div>
+            </div>
+            {line ? <Line/> : "" }
+        </div>
+    );
+}
+
+export default Activity;
